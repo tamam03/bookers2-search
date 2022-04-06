4138d9e892ef05eacba48b6d3427181af61d3a94
tamam03 <tamam0308@gmail.com>
2022-04-06 07:27:22 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' =>  'relationships#followers', as: 'followers'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
+  end
+  get "search" => "searches#search"
 end
